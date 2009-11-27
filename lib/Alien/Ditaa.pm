1f14028b086135643933ee4050f603360611fabb
Tomas Doran (t0m) <t0m@state51.co.uk>
2009-11-27 11:04:25 +0000

    Make work

--- lib/Alien/Ditaa.pm
+++ lib/Alien/Ditaa.pm
@@ -1,15 +1,26 @@
 package Alien::Ditaa;
 use Moose;
 use Method::Signatures::Simple;
-use File::ShareDir qw/dist_dir/;
-use Path::Class qw/dir/;
+use File::ShareDir qw/module_dir/;
+use Path::Class qw/dir file/;
+use File::Which qw/which/;
 use namespace::autoclean;
 
 our $VERSION = '0.001';
 $VERSION = eval $VERSION;
 
 method get_installed_dir {
-    return dir( dist_dir('Alien-Ditaa'), 'ditaa0_9' );
+    return dir( module_dir('Alien::Ditaa') );
+}
+
+method get_jar {
+    file( $self->get_installed_dir, 'ditaa0_9.jar' );
+}
+
+method run_ditaa {
+    shift;
+    my $java = which 'java';
+    exec $java, '-jar', $self->get_jar, @_;
 }
 
 __PACKAGE__->meta->make_immutable;
